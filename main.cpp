954f747ff480b8f2d11215602e08eb39f9f901e0
Velocifero1052 <commelevent1052@yandex.ru>
2023-04-26 16:28:45 +0500

    average temp 2

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <numeric>
 
 using std::vector;
 using std::cout;
@@ -8,9 +9,39 @@ using std::out_of_range;
 using std::invalid_argument;
 using std::ostream;
 using std::istream;
-
+using std::accumulate;
 
 int main() {
+
+    size_t size_of_vector;
+    cin >> size_of_vector;
+
+    vector<int64_t> temps(size_of_vector);
+
+    for(auto& elem: temps){
+        cin >> elem;
+    }
+
+    int64_t average = accumulate(temps.begin(), temps.end(),
+                                      decltype(temps)::value_type(0));
+    average /= static_cast<int64_t>(temps.size());
+
+    vector<size_t> res;
+    for(size_t i = 0; i < temps.size(); i++){
+        if(temps[i] > average)
+            res.push_back(i);
+    }
+
+    cout << res.size() << '\n';
+    bool first = true;
+    for(const auto& elem: res){
+        if (first) {
+            cout << elem;
+            first = false;
+        }else{
+            cout << " " << elem;
+        }
+    }
 
     return 0;
 }
