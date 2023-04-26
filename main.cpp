d6c68eecf6f05b0b5408bd30748e15071ff0f608
Velocifero1052 <commelevent1052@yandex.ru>
2023-04-26 17:45:54 +0500

    canister weight task completed

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <numeric>
+#include <stdint.h>
 
 using std::vector;
 using std::cout;
@@ -13,7 +14,14 @@ using std::accumulate;
 
 int main() {
 
+    uint64_t n, r;
+    uint64_t res = 0, w, h, d;
+    cin >> n >> r;
 
-
+    for(uint64_t i = 0; i < n; i++){
+        cin >> w >> h >> d;
+        res += w * h * d * r;
+    }
+    cout << res << '\n';
     return 0;
 }
