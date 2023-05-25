6b235b5498ead0ca9904bde1bfdb934741d5b011
Velocifero1052 <commelevent1052@yandex.ru>
2023-05-25 12:32:16 +0500

    string grouping starter

--- main.cpp
+++ main.cpp
@@ -10,8 +10,21 @@
 
 using namespace std;
 
+template <typename RandomIt>
+pair<RandomIt, RandomIt> FindStartsWith(RandomIt range_begin, RandomIt range_end, char prefix) {
+    return make_pair(range_begin, range_end);
+}
+
 int main() {
 
+    const vector<string> sorted_strings = {"moscow", "murmansk", "vologda"};
+
+    const auto m_result =
+            FindStartsWith(begin(sorted_strings), end(sorted_strings), 'm');
+    for (auto it = m_result.first; it != m_result.second; ++it) {
+        cout << *it << " ";
+    }
+    cout << endl;
 
     return 0;
 }
