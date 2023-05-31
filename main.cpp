a83d5900203405225416b9fba9b63f74c9626979
Velocifero1052 <commelevent1052@yandex.ru>
2023-05-31 17:35:39 +0500

    final project completed main added

--- main.cpp
+++ main.cpp
@@ -1,18 +1,71 @@
-#include <iostream>
-#include <memory>
-#include <vector>
+#include "database.h"
+#include "date.h"
+#include "condition_parser.h"
+#include "node.h"
 #include "test_runner.h"
+
+#include <iostream>
+#include <stdexcept>
 
 using namespace std;
 
-
 string ParseEvent(istream& is) {
-    // Реализуйте эту функцию
+    while (isspace(is.peek())) {
+        is.ignore();
+    }
+    string event;
+    getline(is, event);
+    return event;
 }
 
 void TestAll();
 
 int main() {
+    TestAll();
+
+    Database db;
+
+    for (string line; getline(cin, line); ) {
+        istringstream is(line);
+
+        string command;
+        is >> command;
+        if (command == "Add") {
+            const auto date = ParseDate(is);
+            const auto event = ParseEvent(is);
+            db.Add(date, event);
+        } else if (command == "Print") {
+            db.Print(cout);
+        } else if (command == "Del") {
+            auto condition = ParseCondition(is);
+            auto predicate = [condition](const Date& date, const string& event) {
+                return condition->Evaluate(date, event);
+            };
+            auto count = db.RemoveIf(predicate);
+            cout << "Removed " << count << " entries" << endl;
+        } else if (command == "Find") {
+            auto condition = ParseCondition(is);
+            auto predicate = [condition](const Date& date, const string& event) {
+                return condition->Evaluate(date, event);
+            };
+
+            const auto entries = db.FindIf(predicate);
+            for (const auto& entry : entries) {
+                cout << entry << endl;
+            }
+            cout << "Found " << entries.size() << " entries" << endl;
+        } else if (command == "Last") {
+            try {
+                cout << db.Last(ParseDate(is)) << endl;
+            } catch (invalid_argument&) {
+                cout << "No entries" << endl;
+            }
+        } else if (command.empty()) {
+            continue;
+        } else {
+            throw logic_error("Unknown command: " + command);
+        }
+    }
 
     return 0;
 }
@@ -38,6 +91,14 @@ void TestParseEvent() {
 void TestAll() {
     TestRunner tr;
     tr.RunTest(TestParseEvent, "TestParseEvent");
-    TestFunc TestParseCondition;
+    tr.RunTest(TestDateOutput, "TestDateOutput");
+    tr.RunTest(TestParseDate, "TestParseDate");
+    tr.RunTest(TestDatabaseAddAndPrint, "TestDatabaseAddAndPrint");
+    tr.RunTest(TestDatabaseFind, "TestDatabaseFind");
+    tr.RunTest(TestDatabaseRemove, "TestDatabaseRemove");
+    tr.RunTest(TestDatabaseLast, "TestDatabaseLast");
+    tr.RunTest(TestDateComparisonNode, "TestDateComparisonNode");
+    tr.RunTest(TestEventComparisonNode, "TestEventComparisonNode");
+    tr.RunTest(TestLogicalOperationNode, "TestLogicalOperationNode");
     tr.RunTest(TestParseCondition, "TestParseCondition");
 }
